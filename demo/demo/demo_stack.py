0972fcb8a60dc7b55bc4ed11a1561e40037813ed
Cer0un0 <wataru.mrt@gmail.com>
2022-08-24 23:36:25 +0900

    [add]: EventBrige->Lambdaを追加

--- demo/demo/demo_stack.py
+++ demo/demo/demo_stack.py
@@ -2,6 +2,8 @@ from constructs import Construct
 from aws_cdk import (
     Stack,
     aws_lambda as _lambda,
+    aws_events as _events,
+    aws_events_targets as _targets,
 )
 
 class DemoStack(Stack):
@@ -10,9 +12,17 @@ class DemoStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # Defines an AWS Lambda resource
-        my_lambda = _lambda.Function(
+        lambda_demofunc1 = _lambda.Function(
             self, 'demofunc1',
-            runtime=_lambda.Runtime.PYTHON_3_7,
+            runtime=_lambda.Runtime.PYTHON_3_8,
             code=_lambda.Code.from_asset('lambda'),
             handler='demofunc1.lambda_handler',
         )
+
+        rule = _events.Rule(
+            self, 'schedule_demofunc1',
+            schedule=_events.Schedule.cron(minute="0", hour="0", month="*", week_day="*", year="*"),
+        )
+        rule.add_target(_targets.LambdaFunction(lambda_demofunc1))
+
+
