090bf85112444909d30c43d50ef59dfd1e097a96
Cer0un0 <wataru.mrt@gmail.com>
2022-08-26 16:51:31 +0900

    add: Lambda -> Budget のPolicyを追加

--- demo/demo/demo_stack.py
+++ demo/demo/demo_stack.py
@@ -5,7 +5,10 @@ from aws_cdk import (
     aws_events as _events,
     aws_events_targets as _targets,
     aws_budgets as _budgets,
+    aws_iam as _iam,
+    Duration,
 )
+
 import os
 from dotenv import load_dotenv
 
@@ -19,25 +22,54 @@ class DemoStack(Stack):
         BUDGET_NAME = os.environ['BUDGET_NAME']
         BUDGET_LIMIT_AMOUNT = int(os.environ['BUDGET_LIMIT_AMOUNT'])
         WEBHOOK_URL = os.environ['WEBHOOK_URL']
+        BUDGET_ROLE = os.environ['READ_BUDGET_ROLE']
 
+        # Create requests layer
+        layer_requests = _lambda.LayerVersion(
+            self, 'requests',
+            compatible_runtimes=[_lambda.Runtime.PYTHON_3_8],
+            code=_lambda.AssetCode('lambda_layer/requests'),
+        )
+
+        # Create pytz layer
+        layer_pytz = _lambda.LayerVersion(
+            self, 'pytz',
+            compatible_runtimes=[_lambda.Runtime.PYTHON_3_8],
+            code=_lambda.AssetCode('lambda_layer/pytz'),
+        )
+
+        # Create submitBudgetsToDiscord Function
         lambda_demofunc1 = _lambda.Function(
             self, 'submitBudgetsToDiscord',
             runtime = _lambda.Runtime.PYTHON_3_8,
             code = _lambda.Code.from_asset('lambda'),
             handler = 'submitBudgetsToDiscord.lambda_handler',
-            environment={
+            environment = {
                 'ACCOUNT_ID': ACCOUNT_ID,
                 'WEBHOOK_URL': WEBHOOK_URL,
                 'BUDGET_NAME': BUDGET_NAME,
             },
+            timeout = Duration.seconds(300),
+            layers = [layer_requests, layer_pytz],
         )
 
+        # Set "budgets:ViewBudget" pocily to Lambda
+        lambda_demofunc1.add_to_role_policy(_iam.PolicyStatement(
+            effect = _iam.Effect.ALLOW,
+            resources=['*'],
+            actions=[
+                    'budgets:ViewBudget'
+                ],
+        ))
+
+        # Create EventBridge rule and set trigger to Lambda
         rule_demofunc1 = _events.Rule(
             self, 'schedule_submitBudgetsToDiscord',
-            schedule = _events.Schedule.cron(minute="0", hour="0", month="*", week_day="*", year="*"),
+            schedule = _events.Schedule.cron(minute='0', hour='0', month='*', week_day='*', year='*'),
         )
         rule_demofunc1.add_target(_targets.LambdaFunction(lambda_demofunc1))
 
+        # Create a Budget referenced by Lambda
         # https://docs.aws.amazon.com/cdk/api/v1/python/aws_cdk.aws_budgets/CfnBudget.html
         budget_demo1 = _budgets.CfnBudget(
             self, 'discordBudget',
@@ -53,3 +85,4 @@ class DemoStack(Stack):
                 budget_name = 'discordBudget',
             ),
         )
+
